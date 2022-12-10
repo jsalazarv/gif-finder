bf02e41d29f3d58f572ae9147dd568ed35a5eee9
jsalazarv <jsalazarv8@gmail.com>
2022-12-10 14:07:45 -0600

    Added new line

--- src/config/services.js
+++ src/config/services.js
@@ -2,7 +2,7 @@ export const endpoints = {
   giphy: {
     config: {
       baseURL: import.meta.env.VITE_GIPHY_API,
-      api_key: import.meta.env.VITE_GIPHY_API_TOKEN,
+      apiKey: import.meta.env.VITE_GIPHY_API_TOKEN,
     },
   },
 };
